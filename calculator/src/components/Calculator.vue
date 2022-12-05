0cd151e61300aba76a83856901af9a9cd3bad8db
why <2687361374@qq.com>
2022-12-05 18:18:06 +0800

    注释添加

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -40,6 +40,7 @@ export default {
     };
   },
   methods: {
+    // 逻辑处理，不同按键触发不同函数
     fn(e) {
       if (e.target.nodeName === "BUTTON") {
         const type = e.target.dataset.type;
@@ -58,27 +59,45 @@ export default {
         }
       }
     },
+    // 清除函数，显示和表示全归零
     clear() {
       this.processScreen = "";
       this.resultScreen = "0";
       this.flag = 0;
     },
+    // 退格函数，
     backspace() {
+      // 退格的是操作符，让标识归零
+      if (
+        ["+", "-", "*", "÷"].indexOf(
+          this.processScreen[this.processScreen.length - 1]
+        ) !== -1
+      ) {
+        this.flag = 0;
+      }
+      // 退格操作
       this.processScreen = this.processScreen.slice(0, -1);
+      // 退格后无数字，显示初始页面，结果显示为零
       if (this.processScreen === "") {
         this.resultScreen = "0";
         this.flag = 0;
       } else {
+        // 有数字取消结果行显示
         this.result = "";
       }
     },
+    // 百分号函数
     percent() {
+      // 先计算后除以100
       this.equal();
       this.resultScreen =
         Math.round((this.resultScreen / 100) * 10 ** 8) / 10 ** 8;
     },
+    // 操作符函数
     operation(operator) {
+      // 屏幕无数字，禁止输入操作符
       if (this.processScreen === "") return;
+      // 如果已输入操作符，先计算一下结果再添加
       if (this.flag !== 0) {
         this.equal();
         this.processScreen = this.resultScreen;
@@ -86,23 +105,26 @@ export default {
       this.processScreen += operator;
       this.flag++;
     },
+    // 等于函数
     equal() {
       let a = /\+|\-|\*|\÷/;
       let arr = this.processScreen.split(a);
       let str = this.processScreen;
       let sign = str.split("").reverse().join("").match(a);
+      // 输入符号，且未输入第二个数字，结果等于第一个数
       if (sign && !arr[1]) {
         this.resultScreen = arr[0];
         return;
       }
+      // 无符号结果为现有数字
       if (!sign) {
         this.resultScreen = this.processScreen;
         return;
       }
+      // 第一个符号前的数为零，（计算出现负数的情况），整理一下数组
       if (!arr[0]) {
         arr.shift();
         arr[0] = -arr[0];
-        console.log(arr[0], arr[1]);
       }
       switch (sign[0]) {
         case "+":
@@ -129,24 +151,33 @@ export default {
           break;
       }
     },
+    // 添加数字的方法
     pushNumber(num) {
       let a = /\+|\-|\*|\÷/;
       let arr = this.processScreen.split(a);
+      // 第一数点的是零且接下来点的不是‘.’，执行一次退格，达到除小数外首个数字不为零
       if (this.processScreen === "0" && num !== ".") {
         this.backspace();
       }
+
       if (this.flag) {
+        // 校验后面数只有一个小数点
         if (arr[1].indexOf(".") !== -1 && num === ".") {
           return;
         }
+        // 第二个数首字母为零，且长度为1
         if (arr[1][0] === "0" && arr[1].length === 1) {
+          // 再次点击零，退格一次
           if (num === "0") {
             this.backspace();
-          } else if (num !== "0" && num !== ".") {
+          }
+          // 再次点击非小数点的数字键，退格一次
+          else if (num !== ".") {
             this.backspace();
           }
         }
       } else {
+        // 校验第一个数只有一个小数点
         if (this.processScreen.indexOf(".") !== -1 && num === ".") {
           return;
         }
